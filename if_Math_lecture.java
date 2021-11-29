fe88a7ebdbe98abb8093539781c8cc52c7462d7f
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-11-29 10:27:12 -0500

    Create if_Math_lecture.java

---
+++ if_Math_lecture.java
@@ -0,0 +1,20 @@
+package com.perscholas.java_basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int count = 10;
+        String msg = "Your count is: ";
+        System.out.println(msg+count);
+//        Math.floor();
+//        Math.random();
+//        Math.abs();
+//        Math.ceil();
+        double ran = Math.floor(Math.random() *10);
+        System.out.println(ran);
+
+
+
+    }
+}
