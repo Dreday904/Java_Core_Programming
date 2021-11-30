763651b31fcc639e727b5c466e1b35be74300fe6
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-11-30 10:11:14 -0500

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+dataType[] nameOfArr = new dataType[];
+//      double[] myList = new double[10];
+//      myList[9]
+//        double lastItem = myList[myList.length-1];
+//        1,6,null
+//        double storeNum = myList[2] = myList[0] + myList[1];
+//        double storeNum = myList(2) = myList(0) + myList(1); cause an error.
+//
+//        double[] myList = {1.9,7.2,9.7,3};
+//        double arrLen = myList.length;
+//        double lastItem = myList[myList.length-1]
+//          System.out.println(myList[myList.length-1]);
+
+        double [] myList = new double[10];
+        myList[0] = 2.8;
+        myList[1] = 6.2;
+        myList[2] = 6.1;
+        myList[3] = 8;
+
+        System.out.println(myList[0]);
