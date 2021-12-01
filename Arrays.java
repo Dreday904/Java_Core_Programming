e5192d6bc3bb84520066e5d91a432193a67ac6e3
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-12-01 09:06:47 -0500

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,4 +1,13 @@
-dataType[] nameOfArr = new dataType[];
+package com.perscholas.java_basics;
+
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//    dataType[] nameOfArr = new dataType[];
 //      double[] myList = new double[10];
 //      myList[9]
 //        double lastItem = myList[myList.length-1];
@@ -11,17 +20,14 @@ dataType[] nameOfArr = new dataType[];
 //        double lastItem = myList[myList.length-1]
 //          System.out.println(myList[myList.length-1]);
 
-        double [] myList = new double[10];
-        myList[0] = 2.8;
-        myList[1] = 6.2;
-        myList[2] = 6.1;
-        myList[3] = 8;
+//        double [] myList = new double[10];
+//        myList[0] = 2.8;
+//        myList[1] = 6.2;
+//        myList[3] = 8;
+//        myList[4] = 6.1;
+//        myList[2] = myList[0]+myList[1];
 
-        System.out.println(myList[0]);
-
-
-
-   for(int i = 0; i < myList.length; i++);{
+////        for(int i = 0; i < myList.length; i++);{
 //////            double printArr = myList[i];
 ////            System.out.println(myList[]);
 ////        }
@@ -34,9 +40,8 @@ dataType[] nameOfArr = new dataType[];
 //            deckCards[i] = i;
 //            System.out.println(i);
 //        }
-           
-           
-             //2D Arrays
+
+//        //2D Arrays
 //        int [][] values = new int [0][0];
 //        int nRows = values.length;
 //        int nColumns = values.length;
@@ -47,23 +52,98 @@ dataType[] nameOfArr = new dataType[];
 //            }
 //        }
         //This is the same as the bottom
-        int[] source = {1,2,3,4,5};
-        int[]targetArr = new int [source.length];
+//        int[] source = {1,2,3,4,5};
+//        int[]targetArr = new int [source.length];
+//
+//        for(int i =0; i < source.length; i++){
+//            targetArr[i] = source[i];
+//            System.out.println(targetArr[i]);
+//        }
+//
+//
+//
+//
+//        int[] vals = {1,2,3,4,5};
+//        int[] copy = vals.clone();
+//        System.out.println(Arrays.toString(copy));
 
-        for(int i =0; i < source.length; i++){
-            targetArr[i] = source[i];
-            System.out.println(targetArr[i]);
-        }
+
+//
+
+//
+//        Scanner input = new Scanner(System.in);
+//        int[] values = new int[5];
+//        for(int i = 0; i <= values.length; i++){
+//            System.out.println("Please enter a number: "+i+": ");
+//            values[i - 1] = input.nextInt();
+//        }
+
+
+//
+//        double[] values = new double[20];
+//        int maxValue = 20;
+//        for(int i = 0; i < values.length; i++){
+//            values[i] = Math.random() * maxValue;
+//        }
+//        System.out.println(Arrays.toString(values));
+
+
+        // adding all the elements in the array
+//        int[] vals = {1,2,3,5,100};
+//        int sum = 0;
+//        for(int i = 0; i < vals.length; i++){
+////            sum = sum+vals[i];
+//            //add the values everytime.
+//            sum+=vals[i];
+//        }
+//        System.out.println(sum);
+
+
+//        finding the largest number in the array
+//
+//        double[] values = {54,87,23,9,24,87};
+//
+//        //this is how we get the maxVal
+//        double maxVal = values[0];
+//        int maxIdx = 0;
+//        for(int i = 0; i < values.length ;i++){
+//            //To get the minimum, just flip the < > symbol
+//            if (values[i] > maxVal){
+//                maxVal = values[i];
+//                maxIdx = i;
+//            }
+//        }
+//        //You'll get 87.0
+//        System.out.println(maxVal);
+
+//        double maxVal = values[0];
+//        for(int i =0; i < values.length; i++){
+//            if (values[i] > maxVal){
+//                maxVal = values[i];
+//                System.out.println(maxVal);
+//            }
+//        }
+
+            //this is how you reverse the values.
+//            int[] values = {87,23,9,24,77};
+//
+//            int[] revArr = new int [values.length];
+//            for(int i = 0, j = revArr.length-1; i < revArr.length; i++, j--){
+//                revArr[i] = values[j];
+//            }
+//        System.out.println(Arrays.toString(revArr));
+
+//          another way to reverse
+//        int revInd = 0;
+//        for (int i = values.length - 1; i >= 0; i--) {
+//            revArr[revInd] = values[i];
+          revInd++;
+      }
 
 
 
-
-        int[] vals = {1,2,3,4,5};
-        int[] copy = vals.clone();
-        System.out.println(Arrays.toString(copy));
+        // int - data types // INTEGER - wrapper classes
 
 
-           
-           
-           
-           
+    }
+}
