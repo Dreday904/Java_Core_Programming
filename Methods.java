d39df65e4fde1581ecfe8a87b7879a192411cc26
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-12-01 10:07:24 -0500

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -6,44 +6,59 @@ import java.util.List;
 
 public class Main {
 
-    //Main method. A block of code. Static
+    //Main method. A block of code. Invoke is the same as calling it.
     public static void main(String[] args) {
+
+        password();
+        password("passwrord12345");
+        password("password12345", 5, false );
+
+
 
 
     }
+
     //A parameter is what in the ();
 
     // Access Specifier is the public.
     //Return type is the void. Can use any datatype. has to return
     //same datatype
     //
-    public String sayMyName(String name){
+    public static String sayMyName(String name){
         return name;
     }
 
-    public String getLocalTime(){
+    public static String getLocalTime(){
         return "invoked by ny of the customer of the class";
 
 
     }
 
     // The Datatype makes it unique.
-    private String password(String password){
+    private String password(String password, int num){
+        return "Only invoked within the class definition";
+    }
+    //invalid method. same number of parameter of the same type. Unless you change the order.
+    private  String password(int num, String userName){
         return "Only invoked within the class definition";
     }
 
-    private String password(){
-        return "Only invoked within the class definition";
+
+    private static void password(String passwrord12345) {
     }
 
 
     //Changed double to int. Why? Method Overloading. using a same method multiple times.
-    private String password(String password, double numberOfChar, boolean isPasswordCorrect ){
+    private static String password(String password, double numberOfChar, boolean isPasswordCorrect ){
         return "Only invoked within the class definition";
     }
 
     //Knows which one to call based on parameter within the parameters. unique
     private String password(double numberOfChar, boolean isPasswordCorrect ){
+        return "Only invoked within the class definition";
+    }
+
+    private static String password(){
         return "Only invoked within the class definition";
     }
 
