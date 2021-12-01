c3b93f1fbae7dd68f0c690fd507594ba2aed42ce
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-12-01 09:46:22 -0500

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -11,6 +11,8 @@ public class Main {
 
 
     }
+    //A parameter is what in the ();
+
     // Access Specifier is the public.
     //Return type is the void. Can use any datatype. has to return
     //same datatype
@@ -21,18 +23,43 @@ public class Main {
 
     public String getLocalTime(){
         return "invoked by ny of the customer of the class";
+
+
+    }
+
+    // The Datatype makes it unique.
+    private String password(String password){
+        return "Only invoked within the class definition";
     }
 
     private String password(){
         return "Only invoked within the class definition";
     }
+
+
+    //Changed double to int. Why? Method Overloading. using a same method multiple times.
+    private String password(String password, double numberOfChar, boolean isPasswordCorrect ){
+        return "Only invoked within the class definition";
+    }
+
+    //Knows which one to call based on parameter within the parameters. unique
+    private String password(double numberOfChar, boolean isPasswordCorrect ){
+        return "Only invoked within the class definition";
+    }
+
+
+
+
     protected String userName(){
         return "method can be invoked from within the class or from " +
                 "within any subclass of the class.";
     }
-    
-    
+
     String myName(){
         return "(default) means that the method can be invoked from within the " +
-                "class or by any other class within the same package that contains this class.";
+                "class or by any other class within the same package that contains this class.\n" +
+                ;
+    }
+
+
 }
