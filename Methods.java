e79b5e0eb3fbfc8a6be27d57d88e66bc73bc2f71
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-12-01 09:32:13 -0500

    Update Methods.java

--- Methods.java
+++ Methods.java
@@ -1,1 +1,33 @@
+package com.perscholas.java_basics;
 
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Main {
+
+    //Main method. A block of code. Static
+    public static void main(String[] args) {
+
+
+    }
+    // Access Specifier is the public.
+    //Return type is the void. Can use any datatype. has to return
+    //same datatype
+    //
+    public String sayMyName(String name){
+        return name;
+    }
+
+    public String getLocalTime(){
+        return "invoked by ny of the customer of the class";
+    }
+
+    private String password(){
+        return "Only invoked within the class definition";
+    }
+    protected String userName(){
+        return "method can be invoked from within the class or from " +
+                "within any subclass of the class.";
+    }
+}
